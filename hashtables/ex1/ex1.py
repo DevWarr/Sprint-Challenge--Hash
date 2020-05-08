1973162282a5a291c9ae19812a4d9db1b25b0cf4
Devin Warrick <dev.warr@gmail.com>
2020-05-08 10:44:24 -0500

    Solves ex1; all tests pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,11 @@
 def get_indices_of_item_weights(weights, length, limit):
 
-    """
-    YOUR CODE HERE
-    """
+    cache = {}
+
+    for i in range(length):
+        if limit - weights[i] in cache:
+            return [i, cache[limit - weights[i]]]
+        else:
+            cache[weights[i]] = i
 
     return None
