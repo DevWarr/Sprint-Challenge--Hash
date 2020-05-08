bfb259d6b7a1dee8baf63829632fa52f9d7527cf
Devin Warrick <dev.warr@gmail.com>
2020-05-08 11:00:28 -0500

    Solves ex2; all tests pass

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,27 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     
-    """
-    YOUR CODE HERE
-    """
+    cache = {}
+    starting_ticket = None
 
+    for i in range(length):
+        ticket = tickets[i]
+        if ticket.source == "NONE":
+            starting_ticket = ticket
+        else:
+            cache[ticket.source] = ticket
+
+    route = [None] * length
+    i = 0
+    while starting_ticket is not None:
+        if i >= length:
+            # If by some magical reason our pointer passes the 
+            # length of the array, append instead of 
+            # " route[i] = " so we don't catch an IndexError
+            route.append(starting_ticket)
+        else:
+            route[i] = starting_ticket.destination
+        starting_ticket = cache.get(starting_ticket.destination)
+        i -= -1 # Because we're arbitrarily fancy
+    
     return route
